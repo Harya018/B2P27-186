c14e4305ce1c256c6072e3e5cb5b59e0ad54db01
Harya018 <haryacr@gmail.com>
2026-03-02 10:31:30 +0530

    UC2: Add hardcoded string palindrome check

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,19 +1,13 @@
 public class PalindromeChecker {
 
-    private static final String APP_NAME = "Palindrome Checker";
-    private static final String APP_VERSION = "1.0.0";
-
     public static void main(String[] args) {
-        displayWelcomeMessage();
-        System.out.println("Application is ready to process palindromes...");
-    }
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
 
-    private static void displayWelcomeMessage() {
-        System.out.println("==================================");
-        System.out.println("Welcome to " + APP_NAME);
-        System.out.println("Version: " + APP_VERSION);
-        System.out.println("==================================");
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
-//-----------------------------
-//-------------------------------
